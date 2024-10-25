3094968b748cc9845cd13ac25170b20103c5ec22
wyw4a <wywkaa66@gmail.com>
2024-10-25 23:29:14 +0100

    Add files via upload

---
+++ login-script.js
@@ -0,0 +1,6 @@
+function openForm(){
+    document.getElementById("loginForm").style.display = "block";
+}
+function closeForm(){
+    document.getElementById("loginForm").style.display = "none";
+}
---
+++ login.php
@@ -0,0 +1,34 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "novobank";
+
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+
+if(isset($_POST['loginid']) && isset($_POST['psw'])) {
+
+$name = $_POST['loginid'];
+$psw = $_POST['psw'];
+
+$sql = "SELECT * from users WHERE login_id='$name' AND password='$psw' ";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0){
+    echo "Zalogowano!";
+} else {
+    echo "Niepoprawny login lub hasło.";
+}
+} else {
+    echo "Wprowadź login i hasło";
+}
+
+
+$conn->close();
+
+?>
